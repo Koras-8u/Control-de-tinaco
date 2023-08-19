fa5935ebe5a8d6f75fdea280b576893eb4aad540
Kevin García Morales <kevwk99@gmail.com>
2023-08-18 18:41:37 -0600

    Clase Relay en vez de Pump
    Relay es el objeto al que está conectado directamente la ESP32, por lo que no importa el dispositivo o actuador que energiza el relevador. En todo caso, al objeto creado de la clase Relay se le asigna el nombre de dicho elemento.

--- main/Relay.cpp
+++ main/Relay.cpp
@@ -1,29 +1,45 @@
-#include "Pump.h"
+#include "Relay.h"
 
 // --------------------------------------------------
 //                    CONSTRUCTORS                    
 // --------------------------------------------------
-Pump::Pump() {}
+Relay::Relay() {}
 
-Pump::Pump(uint8_t pin) {
+Relay::Relay(uint8_t pin, String device_name) {
     this->pin = pin;
+    this->device_name = device_name;
 }
 
-Pump::~Pump() {}
+Relay::Relay(uint8_t pin) {
+    Relay(pin, "Relay");
+}
+
+Relay::~Relay() {}
 
 // --------------------------------------------------
 //                     FUNCTIONS
 // --------------------------------------------------
 
-void Pump::pumpsWater(bool status) {
+void Relay::activates(bool status, String device_name) {
         digitalWrite(pin, !status);
         if(status != last_status) {
-            Serial.print("|\t-Pump:");
+            Serial.print("|\t-" + device_name + ":");
             Serial.println(status ? " ON" : " OFF");
             last_status = status;
         }
 }
 
+void Relay::activates(bool status) {
+    activates(status, device_name);
+}
+
 // --------------------------------------------------
 //                 GETTERS & SETTERS                 
 // --------------------------------------------------
+void Relay::setPin(uint8_t pin) {
+    this->pin = pin;
+}
+
+void Relay::setDeviceName(String device_name) {
+    this->device_name = device_name;
+}
